cae1cfb11044d1cd70e8bc9f22fe09f94ca87975
JUB_Yoush <yoush1337@gmail.com>
2024-10-07 15:04:28 -0400

    backend for setup done

--- backend/app.py
+++ backend/app.py
@@ -5,15 +5,26 @@ import json
 
 app = Flask(__name__)
 cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+app.config["CORS_HEADERS"] = "Content-Type"
 
-mongoClient = MongoClient('mongodb://127.0.0.1:27017')
-db = mongoClient.get_database('names_db')
+mongoClient = MongoClient("mongodb://127.0.0.1:27017")
+db = mongoClient.get_database("msg_db")
+msg_col = db.get_collection("msg_col")
 
 
-@app.route("/")
-def hello_world():
-    return "amongus (server)"
+@app.route("/addmsg/<msg>/")
+def addmsg(msg):
+    msg_col.insert_one({"msg": msg.lower()})
+    return redirect(url_for("getmsgs"))
+
+
+@app.route("/getnames/")
+def getmsgs():
+    msg_json = []
+    if msg_col.find({}):
+        for msg in msg_col.find({}).sort("msg"):
+            msg_json.append({"msg": msg["msg"], "id": str(msg["_id"])})
+    return json.dumps(msg_json)
 
 
 if __name__ == "__main__":
