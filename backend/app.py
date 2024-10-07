a5c31867aa58f5d9a4423a37d70c999f81cafb35
JUB_Yoush <yoush1337@gmail.com>
2024-10-07 14:44:48 -0400

    making setup

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
+from pymongo import MongoClient
+from flask_cors import CORS, cross_origin
+import json
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+mongoClient = MongoClient('mongodb://127.0.0.1:27017')
+db = mongoClient.get_database('names_db')
 
 
 @app.route("/")
