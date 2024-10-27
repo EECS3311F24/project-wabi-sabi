0d345d18a35bd207f2fe667f355a09681de65c6e
JUB_Yoush <yoush1337@gmail.com>
2024-10-27 19:59:10 -0400

    making flask server archetecture

--- backend/app/auth/models.py
+++ backend/app/auth/models.py
@@ -3,11 +3,3 @@ from flask_pymongo import pymongo
 from flask import current_app
 from mongoengine import Document, StringField, IntField, connect
 
-
-#class User(Document):
-    #username = StringField(required=True, unique=True)
-    #email = StringField(required=True, unique=True)
-
-    #@staticmethod
-    #def find_by_username(username):
-        #return mongo.db.users.find_one({"username": username})
--- backend/app/main/__init__.py
+++ backend/app/main/__init__.py
@@ -3,7 +3,6 @@ from flask import Blueprint
 main = Blueprint("main", __name__)
 
 from .tasks import tasks
-
 main.register_blueprint(tasks)
 
 from . import routes
