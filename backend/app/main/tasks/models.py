2caad6fa75144043b9e07af6a0d54b67bc8a1493
JUB_Yoush <yoush1337@gmail.com>
2024-10-27 19:59:10 -0400

    making flask server archetecture

---
+++ backend/app/main/tasks/models.py
@@ -0,0 +1,15 @@
+
+class Task:
+    def __init__():
+        self.taskID = generate_task_id()
+        self.isSubTask
+        self.description
+        self.due_date
+        self.tag
+        self.status
+        self.subtasks = []
+
+
+    # implement
+    def generate_task_id():
+        pass
