e964924862733d44ff4b45a2c786fe83ea916be2
Fred Hong <fredh9@my.yorku.ca>
2024-10-28 16:05:49 -0400

    UPDATED app.py

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
-from flask import Flask, redirect, url_for, request, jsonify
+from flask import Flask, redirect, url_for, request
 from pymongo import MongoClient, errors
 from flask_swagger_ui import get_swaggerui_blueprint
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 import json
 import time
 
