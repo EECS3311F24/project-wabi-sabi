ed49114e80974ff0dd47ae605e21b6094a5a14ef
jard-io <50737321+jard-io@users.noreply.github.com>
2024-10-27 23:52:44 -0400

    Create signup form

---
+++ frontend/src/components/SignupSuccessCard.tsx
@@ -0,0 +1,17 @@
+import { Card, CardContent } from './ui/card';
+
+/**
+ * The confirmation that pops up if the user signed up successfully.
+ * @returns
+ */
+const SignupSuccess = () => {
+  return (
+    <Card>
+      <CardContent>
+        <h2>yay!</h2>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default SignupSuccess;
