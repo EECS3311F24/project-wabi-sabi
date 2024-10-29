8c79773d04468fe7da7fe8c8593aef4d9f6a102c
JUB-Yoush <yoush1337@gmail.com>
2024-10-29 14:44:45 -0400

    task model

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,5 @@
 from mongoengine import *
+from enum import Enum
 
 # all models should go here?
 
@@ -9,3 +10,18 @@ class User(Document):
 
     def __str__(self):
         return self.username
+
+
+class TASK_STATUS(Enum):
+    TODO = "Todo"
+    IN_PROGRESS = "In Progress"
+    FINISHED = "Finished"
+
+class Task:
+    def __init__(description):
+        self.taskId = ""
+        self.is_sub_task = False
+        self.description = description
+        self.due_date = ""
+        self.tag = ""
+        self.status = TASK_STATUS.TODO
