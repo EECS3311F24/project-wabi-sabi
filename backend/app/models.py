6bca585922be3f69e73ca00c2c0629c1e5387bed
JUB_Yoush <yoush1337@gmail.com>
2024-10-30 21:01:26 -0400

    swagger documented

--- backend/app/models.py
+++ backend/app/models.py
@@ -4,6 +4,7 @@ from enum import Enum
 # all models should go here?
 
 
+
 class Task(Document):
     STATUS_TODO = "Todo"
     STATUS_IN_PROGRESS = "In Progress"
@@ -12,9 +13,9 @@ class Task(Document):
     is_sub_task = BooleanField(required=True)
     text = StringField(required=True)
     due_date = DateTimeField()
-    tag = ObjectIdField()
+    tag = ObjectIdField() # change to reference field once tag object exists
     status = StringField(required=True)
-    # sub_tasks = ListField(ReferenceField())
+    #sub_tasks = ListField(ReferenceField())
 
     def update_status(new_status):
         status = new_status
@@ -23,20 +24,21 @@ class Task(Document):
         return self.text
 
     def to_json(self):
-        # sub_task_json = []
-        # for task in self.sub_tasks:
-        # sub_task_json.append(task.to_json())
+        #sub_task_json = []
+        #for task in self.sub_tasks:
+            #sub_task_json.append(task.to_json())
         jsonified = {
-            "id": str(self.id),
-            "is_sub_task": str(self.is_sub_task),
-            "text": str(self.text),
-            "due_date": str(self.due_date) if self.due_date else None,
-            "tag": str(self.tag) if self.tag else None,
-            "status": str(self.status),
+            'id': str(self.id),
+            'is_sub_task':str(self.is_sub_task),
+            'text':str(self.text),
+            'due_date':str(self.due_date) if self.due_date else None,
+            'tag':str(self.tag) if self.tag else None,
+            'status':str(self.status),
             #'sub_tasks': str(sub_task_json),
         }
         print(jsonified)
         return jsonified
+
 
 
 class User(Document):
