2db175381609387c72bac01e13dbc57c587f380d
amanuelak <amanuel9@my.yorku.ca>
2024-11-21 14:00:11 -0500

    added a state to change the arrow type on a click

--- frontend/src/components/SortTable.tsx
+++ frontend/src/components/SortTable.tsx
@@ -1,4 +1,6 @@
+import { useState} from 'react';
 import { Button } from './ui/button';
+import { ArrowDown, ArrowUp, ChevronsUpDown} from "lucide-react"
 import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from './ui/dropdown-menu';
 
 interface SortTableProps {
@@ -6,21 +8,33 @@ interface SortTableProps {
 }
 
 export function TaskTableHeader({ title}: SortTableProps) {
+  const[arrowType, setArrowType] = useState<"asc" | "desc" | null>(null); // state to track the arrow type
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="sm" className="flex items-center space-x-2">
           <span>{title}</span>
+            {arrowType === "desc" ? (
+              <ArrowDown />
+            ) : arrowType === "asc" ? (
+              <ArrowUp />
+            ) : (
+              <ChevronsUpDown />
+            )}
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="start">
         <DropdownMenuItem
+        onClick={() => setArrowType("asc")}        
         >
-          Sort Ascending
+        <ArrowUp className="h-3.5 w-3.5 text-muted-foreground/70" />
+          Asc
         </DropdownMenuItem>
         <DropdownMenuItem  
+        onClick={() => setArrowType("desc")}        
         >
-          Sort Descending
+        <ArrowDown className="h-3.5 w-3.5 text-muted-foreground/70" />
+          Desc
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
